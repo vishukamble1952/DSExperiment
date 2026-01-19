f21f2c1a1abc9ddd437c42527d17f35d7bae294a
vishukamble1952 <vishukamble1952@gmail.com>
2026-01-19 22:41:21 +0530

    Create Exp1.cpp

---
+++ Exp1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ int a[3];
+
+ cout<<"Enter numbers:";
+ for(int i=0;i<3;i++){
+        cin>>a[i];
+    }
+ cout<<"The numbers :"<<endl;
+ for(int i=0;i<3;i++){
+    cout<<a[i]<<" ";
+ }
+return 0;
+}
+
+
